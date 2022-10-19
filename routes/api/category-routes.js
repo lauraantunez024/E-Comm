02749e05076250f451c6c2052fc05214eba7a70e
Laura Antunez <lauraantunez024@gmail.com>
2022-10-18 20:19:14 -0400

    fixed not being able to update category_name

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -38,8 +38,16 @@ router.put('/:id', (req, res) => {
     where: {
       id: req.params.id,
     }
+  }).then((a) => {
+    res.json(a)
+  }).catch((err) => {
+    res.json(err)
   })
 });
+
+
+
+
 
 router.delete('/:id', (req, res) => {
   Category.destroy({
